542e0b64518ba96a33e8bd78a5230c38eebdda46
Andrey Lukin <aimm.legate@gmail.com>
2019-01-15 21:01:50 +0500

    s5l52

--- src/prisma.js
+++ src/prisma.js
@@ -5,10 +5,42 @@ const prisma = new Prisma({
   endpoint: "http://localhost:4466"
 });
 
-prisma.query
-  .users(null, "{ id name }")
-  .then(data => console.log(JSON.stringify(data, null, 2)));
+// prisma.query
+//   .users(null, "{ id name posts { id title } }")
+//   .then(data => console.log(JSON.stringify(data, null, 2)));
 
-prisma.query
-  .comments(null, "{ id text author { id name } }")
+// prisma.query
+//   .comments(null, "{ id text author { id name } }")
+//   .then(data => console.log(JSON.stringify(data, null, 2)));
+
+// prisma.mutation
+//   .createPost(
+//     {
+//       data: {
+//         title: "My new post a",
+//         body: "text",
+//         published: false,
+//         author: {
+//           connect: {
+//             id: "cjqwg53lx000w0734p813nf6v"
+//           }
+//         }
+//       }
+//     },
+//     "{ id, title, body, published }"
+//   )
+//   .then(data => console.log(JSON.stringify(data, null, 2)));
+
+prisma.mutation
+  .updatePost(
+    {
+      data: { published: false, body: "aaaaahuina" },
+      where: { id: "cjqxxto3a000k0734yan2tqgf" }
+    },
+    "{ id title body published }"
+  )
+  .then(data => {
+    console.log(JSON.stringify(data, null, 2));
+    return prisma.query.posts(null, "{ id title body published }");
+  })
   .then(data => console.log(JSON.stringify(data, null, 2)));
