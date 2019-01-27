c07291e7136316fa640fcc77ac98d778ac04f754
Andrey Lukin <aimm.legate@gmail.com>
2019-01-27 14:31:51 +0500

    s6l66

--- src/prisma.js
+++ src/prisma.js
@@ -2,7 +2,8 @@ import { Prisma } from "prisma-binding";
 
 const prisma = new Prisma({
   typeDefs: "src/generated/prisma.graphql",
-  endpoint: "http://localhost:4466"
+  endpoint: "http://localhost:4466",
+  secret: "A670E5C79BAE9BDD9C9921B12B018F8C29679E5F06E425290C827E6E4743219B"
 });
 
 export { prisma as default };
