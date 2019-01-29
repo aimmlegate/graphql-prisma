832843fe28465fad30aef0a9a3f5ff390a967b0e
Andrey Lukin <aimm.legate@gmail.com>
2019-01-29 20:46:23 +0500

    s6l77

--- src/prisma.js
+++ src/prisma.js
@@ -1,63 +1,11 @@
 import { Prisma } from "prisma-binding";
+import { fragmentReplacements } from "./resolvers";
 
 const prisma = new Prisma({
   typeDefs: "src/generated/prisma.graphql",
+  fragmentReplacements,
   endpoint: "http://localhost:4466",
   secret: "A670E5C79BAE9BDD9C9921B12B018F8C29679E5F06E425290C827E6E4743219B"
 });
 
 export { prisma as default };
-
-// const createPostForUser = async (authorId, data) => {
-//   const isUserExist = await prisma.exists.User({ id: authorId });
-
-//   if (!isUserExist) {
-//     throw new Error("User not found");
-//   }
-
-//   const post = await prisma.mutation.createPost(
-//     {
-//       data: {
-//         ...data,
-//         author: {
-//           connect: {
-//             id: authorId
-//           }
-//         }
-//       }
-//     },
-//     "{ author { id name email posts { id title published } } }"
-//   );
-
-//   return post;
-// };
-
-// const updatePostForUser = async (postId, data) => {
-//   const isPostExist = await prisma.exists.Post({ id: postId });
-
-//   if (!isPostExist) {
-//     throw new Error("Post not found");
-//   }
-
-//   const post = await prisma.mutation.updatePost(
-//     {
-//       data: { ...data },
-//       where: {
-//         id: postId
-//       }
-//     },
-//     "{ author { id name email posts { id title published } } }"
-//   );
-
-//   return post;
-// };
-
-// // createPostForUser("cjqwg53lx000w0734p813nf6v", {
-// //   title: "Great",
-// //   body: "AAAAAA",
-// //   published: true
-// // }).then(user => console.log(JSON.stringify(user, null, 2)));
-
-// updatePostForUser("cjqxx8qcy000a073428kth0og", { title: "HAAAA" }).then(user =>
-//   console.log(JSON.stringify(user, null, 2))
-// );
--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -1,3 +1,32 @@
-const User = {};
+import getUserId from "../utils/getUserId";
+import { Prisma } from "prisma-binding";
+
+const User = {
+  email: {
+    fragment: "fragment userId on User { id }",
+    resolve: (parent, args, { req }, info) => {
+      const userId = getUserId(req, false);
+
+      if (userId && userId === parent.id) {
+        return parent.email;
+      }
+
+      return null;
+    }
+  },
+  posts: {
+    fragment: "fragment userId on User { id }",
+    resolve: (parent, args, { prisma, req }, info) => {
+      return prisma.query.posts({
+        where: {
+          published: true,
+          author: {
+            id: parent.id
+          }
+        }
+      });
+    }
+  }
+};
 
 export { User as default };
