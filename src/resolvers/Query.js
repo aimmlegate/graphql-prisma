8506cd8e2e31622d7ce67f5f66364fcc9e75b740
Andrey Lukin <aimm.legate@gmail.com>
2019-01-30 20:23:23 +0500

    s7l84

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -7,19 +7,20 @@ const Query = {
   },
   users: (parent, args, { prisma }, info) => {
     const constructQuery = args => {
+      const pagination = {
+        first: args.first,
+        skip: args.skip,
+        after: args.after
+      };
       if (args.query) {
         return {
           where: {
             OR: [{ name_contains: args.query }]
           },
-          first: args.first,
-          skip: args.skip
+          ...pagination
         };
       }
-      return {
-        first: args.first,
-        skip: args.skip
-      };
+      return pagination;
     };
 
     const query = constructQuery(args);
@@ -28,17 +29,21 @@ const Query = {
   },
   posts: (parent, args, { prisma }, info) => {
     const constructQuery = args => {
+      const pagination = {
+        first: args.first,
+        skip: args.skip,
+        after: args.after
+      };
       if (args.query) {
         return {
           where: {
             published: true,
             OR: [{ title_contains: args.query }, { body_contains: args.query }]
           },
-          first: args.first,
-          skip: args.skip
+          ...pagination
         };
       }
-      return { where: { published: true }, first: args.first, skip: args.skip };
+      return { where: { published: true }, ...pagination };
     };
 
     const query = constructQuery(args);
@@ -48,22 +53,33 @@ const Query = {
   myPosts: async (parent, args, { prisma, req }, info) => {
     const userId = getUserId(req);
     const constructQuery = args => {
+      const pagination = {
+        first: args.first,
+        skip: args.skip,
+        after: args.after
+      };
       if (args.query) {
         return {
           where: {
             id: { author: { author: { id: userId } } },
             OR: [{ title_contains: args.query }, { body_contains: args.query }]
-          }
+          },
+          ...pagination
         };
       }
-      return { where: { author: { id: userId } } };
+      return { where: { author: { id: userId }, ...pagination } };
     };
     console.log(constructQuery(args));
     const posts = await prisma.query.posts(constructQuery(args), info);
     return console.log(posts) || posts;
   },
   comments: (parent, args, { prisma }, info) => {
-    return prisma.query.comments(null, info);
+    const pagination = {
+      first: args.first,
+      skip: args.skip,
+      after: args.after
+    };
+    return prisma.query.comments({ ...pagination }, info);
   },
   post: async (parent, { id }, { prisma, req }, info) => {
     const userId = getUserId(req, false);
