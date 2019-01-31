48f11041a2b2247981e0be34c61f47077a5cb1ff
Andrey Lukin <aimm.legate@gmail.com>
2019-01-31 21:16:20 +0500

    s8l92

--- src/prisma.js
+++ src/prisma.js
@@ -1,13 +1,11 @@
 import { Prisma } from "prisma-binding";
 import { fragmentReplacements } from "./resolvers";
 
-console.log(process.env.PRISMA_ENDPOINT);
-
 const prisma = new Prisma({
   typeDefs: "src/generated/prisma.graphql",
   fragmentReplacements,
   endpoint: process.env.PRISMA_ENDPOINT,
-  secret: "A670E5C79BAE9BDD9C9921B12B018F8C29679E5F06E425290C827E6E4743219B"
+  secret: process.env.PRISMA_SECRET
 });
 
 export { prisma as default };
--- src/utils/tokenSecret.js
+++ src/utils/tokenSecret.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
-export const JWT_SECRET = "secrethurrdurr";
+export const JWT_SECRET = process.env.JWT_SECRET;
 
 export const OPTIONS = { expiresIn: "30d" };
 
