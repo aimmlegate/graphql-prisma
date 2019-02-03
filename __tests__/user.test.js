6e3a97a0f0d09e363bca606caa05bec98dc1c8a7
Andrey Lukin <aimm.legate@gmail.com>
2019-02-03 18:26:24 +0500

    s9l103

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -74,3 +74,36 @@ test("Should create a new user", async () => {
 
   expect(isUserCreated).toBe(true);
 });
+
+test("Should expose public author profiles", async () => {
+  const getUsers = gql`
+    query {
+      users {
+        id
+        name
+        email
+      }
+    }
+  `;
+
+  const response = await client.query({ query: getUsers });
+
+  expect(response.data.users.length).toBe(1);
+  expect(response.data.users[0].email).toBe(null);
+  expect(response.data.users[0].name).toBe("Jen");
+});
+
+test("Should expose public posts", async () => {
+  const getPosts = gql`
+    query {
+      posts {
+        id
+        title
+      }
+    }
+  `;
+
+  const response = await client.query({ query: getPosts });
+
+  expect(response.data.posts.length).toBe(1);
+});
